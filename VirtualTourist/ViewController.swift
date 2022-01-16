64884793aeb44fb3cb351f1bc7358fc3e7ca5f79
Savyo Brenner <savyobrenner@gmail.com>
2022-01-16 14:03:32 +0100

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Brenner on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
